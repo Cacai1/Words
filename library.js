b1f28cf76a601aef60dfd8a21184b9bc6b9bba68
Cacai1 <147081699+Cacai1@users.noreply.github.com>
2023-10-16 13:26:19 -0600

    Update library.js
    Added the functions of addWord getWordIndex

--- library.js
+++ library.js
@@ -1,1 +1,38 @@
+//Basically just adds a word to your library
+function addWord(library, word){
+  if(library == undefined){
+    console.log("Remember to insert an array into addWord function");
+    return;
+  }
 
+  if(word == undefined){
+    console.log("Remember to insert a word which is a string into addWord function");
+    return;
+  }
+
+  library.push(word);
+}
+
+//Gets the index of a word inside your word library
+function getWordIndex(library, word){
+  if(library == undefined){
+    console.log("Remember to insert an array into getWordIndex function");
+    return("Nothing");
+  }
+
+  if(word == undefined){
+    console.log("Remember to insert a word which is a string into getWordIndex function");
+    return("Nothing");
+  }
+
+  for(i in library){
+    if(library[i] == word){
+      return(i);
+    }
+    else{
+     continue; 
+    }
+  }
+}
+
+//
